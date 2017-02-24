9c02c87d11fb583a57840e1cf42d314106618581
Anastasiia <wanderingknight13@gmail.com>
2017-02-24 16:52:20 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//for (var i = 100; i >= 2; i-=2) {
+    document.write('<br>'+i);
+}
+
+// 100 1 --
+
+var cats= ['barsik', 'tuzik', 'murchuk'];
+cats.push('pushok');
+
+document.write(cats.length[i]);
+document.write('b');
+
+for (var i = 0; i < cats.length; i++) {
+    
+    document.write(cats.[i]);
+    document.write('<b>');
+}
