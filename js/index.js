6c25d42c2bc80d94daceba9116758e4e68df31f8
Anastasiia <wanderingknight13@gmail.com>
2017-02-27 11:48:19 +0000

    Remove old code

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 var position = Number(prompt('Enter number'));
 
-function fibonacci(x) {
+function fibonacci(position) {
     var numbers = [];
 
     numbers[0] = 1;
     numbers[1] = 1;
 
-    for (var n = 2; n < x; n++) {
+    for (var n = 2; n < position; n++) {
         numbers[n] = numbers[n - 1] + numbers[n - 2];
     }
 
-    return numbers[x - 1];
+    return numbers[position - 1];
 }
 
 document.write(fibonacci(position) + "<br>");
-document.write(fibonacci(8));
+
